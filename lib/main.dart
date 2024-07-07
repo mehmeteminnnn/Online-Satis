1b15a78268c219666de3825483370bbcbb73e10d
Mehmet Emin <mhmtmntok@gmail.com>
2024-07-07 17:44:41 +0300

    ana ekran tasarımı güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_satis/widgets/ana_sayfa_urun_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,6 +7,13 @@ void main() {
 
 DateTime hedef_zaman = DateTime(2024, 12, 29, 12);
 Duration kalan_zaman = hedef_zaman.difference(DateTime.now());
+List<String> kategoriler = [
+  "FashionCart",
+  "AppliancesCart",
+  "BeautyCart",
+  "ElectronicCart",
+  "FurnitureCart",
+];
 
 class MyApp extends StatelessWidget {
   @override
@@ -14,7 +22,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Ana Sayfa"),
+          title: Text(
+            "Ana Sayfa",
+            style: TextStyle(fontFamily: "Inter"),
+          ),
           actions: [
             IconButton(
               icon: Icon(Icons.notifications_none_outlined),
@@ -31,132 +42,52 @@ class MyApp extends StatelessWidget {
           ],
         ),
         drawer: Drawer(),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                    ),
-                    labelText: "Ara...",
-                    prefixIcon: Icon(Icons.search)),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 2,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 10.5, 0),
-                    child: SizedBox(
-                      width: 257.8,
-                      child: Text(
-                        'Categories',
-                        style: TextStyle(
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14,
-                          height: 1.5,
-                          letterSpacing: 0.1,
-                          color: Color(0xFF1F2937),
-                        ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
                       ),
-                    ),
-                  ),
-                  Spacer(
-                    flex: 1,
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 1.5, 0, 1.5),
-                    child: Text(
-                      'View All ->',
-                      style: TextStyle(
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 12,
-                        height: 1.5,
-                        color: Color(0xFF6B7280),
-                      ),
-                    ),
-                  ),
-                ],
+                      labelText: "Ara...",
+                      prefixIcon: Icon(Icons.search)),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  for (int i = 0; i < 10; i++)
-                    Column(
-                      children: [
-                        Image(
-                          image: AssetImage('assets/Vector.png'),
-                          width: 45,
-                          height: 45,
-                        ),
-                        Text("Moda"),
-                      ],
-                    )
-                ],
-              ),
-            ),
-            SizedBox(height: 16),
-            SizedBox(
-              width: 410,
-              height: 154,
-              child: PageView(
-                children: [
-                  Image(
-                    image: AssetImage('assets/Banner.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  Image(
-                    image: AssetImage('assets/Slider2.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                  Image(
-                      image: AssetImage('assets/banner2.png'),
-                      fit: BoxFit.cover),
-                ],
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              child: SizedBox(
-                width: 410,
+              SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                      margin: EdgeInsets.fromLTRB(0, 0, 10.5, 0),
                       child: SizedBox(
                         width: 257.8,
                         child: Text(
-                          'Deal of the day',
+                          'Kategoriler',
                           style: TextStyle(
                             fontFamily: "Inter",
                             fontWeight: FontWeight.w600,
                             fontSize: 14,
                             height: 1.5,
                             letterSpacing: 0.1,
-                            color: Colors.black,
+                            color: Color(0xFF1F2937),
                           ),
                         ),
                       ),
                     ),
+                    Spacer(
+                      flex: 1,
+                    ),
                     Container(
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
                       margin: EdgeInsets.fromLTRB(0, 1.5, 0, 1.5),
                       child: Text(
-                        'View All ->',
+                        'Hepsini Gör ->',
                         style: TextStyle(
                           fontFamily: 'Inter',
                           fontWeight: FontWeight.w400,
@@ -169,35 +100,257 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 14.0, vertical: 2),
-                  child: Container(
-                    height: 30,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: const Color.fromARGB(255, 239, 68, 68),
+              SizedBox(height: 16),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    for (int i = 0; i < kategoriler.length; i++)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 9),
+                        child: Column(
+                          children: [
+                            Image(
+                              image: AssetImage('assets/${kategoriler[i]}.png'),
+                            ),
+                            Text(
+                                "${kategoriler[i].substring(0, kategoriler[i].length - 4)}"),
+                          ],
+                        ),
+                      )
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                width: 410,
+                height: 154,
+                child: PageView(
+                  children: [
+                    Image(
+                      image: AssetImage('assets/Banner.png'),
+                      fit: BoxFit.cover,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Text(
-                        " ${kalan_zaman.inHours % 24} HRS ${kalan_zaman.inMinutes % 60} MIN ${kalan_zaman.inSeconds % 60} SEC",
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255)),
+                    Image(
+                      image: AssetImage('assets/Slider2.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                    Image(
+                        image: AssetImage('assets/banner2.png'),
+                        fit: BoxFit.cover),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                child: SizedBox(
+                  width: 410,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: SizedBox(
+                          width: 257.8,
+                          child: Text(
+                            'Günün Fırsatları',
+                            style: TextStyle(
+                              fontFamily: "Inter",
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              height: 1.5,
+                              letterSpacing: 0.1,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        margin: EdgeInsets.fromLTRB(0, 1.5, 0, 1.5),
+                        child: Text(
+                          'Hepsini Gör ->',
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                            height: 1.5,
+                            color: Color(0xFF6B7280),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 14.0, vertical: 2),
+                    child: Container(
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: const Color.fromARGB(255, 239, 68, 68),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text(
+                          " ${kalan_zaman.inHours % 24} HRS ${kalan_zaman.inMinutes % 60} MIN ${kalan_zaman.inSeconds % 60} SEC",
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
                       ),
                     ),
                   ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset('assets/Running.png'),
+                            Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Text("Yürüyüş Ayakkabısı"),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: const Color.fromARGB(255, 239, 68, 68),
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Text(
+                                  "Upto 40% OFF",
+                                  style: TextStyle(
+                                      color: Color.fromRGBO(255, 255, 255, 1)),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          children: [
+                            Image.asset('assets/Sneakers.png'),
+                            Text("Ürün Adı"),
+                            Text("Ürün Fiyatı"),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Image.asset('assets/Speaker.png'),
+                            Text("Ürün Adı"),
+                            Text("Ürün Fiyatı"),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          children: [
+                            Image.asset('assets/Wrist.png'),
+                            Text("Ürün Adı"),
+                            Text("Ürün Fiyatı"),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                    child: SizedBox(
+                      width: 257.8,
+                      child: Text(
+                        'En Çok Satan Ayakkabılar',
+                        style: TextStyle(
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w600,
+                          fontSize: 12.5,
+                          height: 1.5,
+                          letterSpacing: 0.1,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    margin: EdgeInsets.fromLTRB(0, 1.5, 0, 1.5),
+                    child: Text(
+                      'Hepsini Gör ->',
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        height: 1.5,
+                        color: Color(0xFF6B7280),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnaSayfaUrunWidget(
+                        baslik: "Nike",
+                        fiyat: 50,
+                        favoriMi: false,
+                        resimYolu: "assets/Nike1.png",
+                        indirimOrani: 50),
+                    AnaSayfaUrunWidget(
+                        baslik: "Nike",
+                        fiyat: 50,
+                        favoriMi: false,
+                        resimYolu: "assets/NikeSky2.png",
+                        indirimOrani: 50),
+                    AnaSayfaUrunWidget(
+                        baslik: "Nike",
+                        fiyat: 50,
+                        favoriMi: false,
+                        resimYolu: "assets/Adidas1.png",
+                        indirimOrani: 50),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/ana_sayfa_urun_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class AnaSayfaUrunWidget extends StatefulWidget {
+  AnaSayfaUrunWidget(
+      {super.key,
+      required this.baslik,
+      required this.fiyat,
+      required this.favoriMi,
+      required this.resimYolu,
+      required this.indirimOrani});
+
+  @override
+  State<AnaSayfaUrunWidget> createState() => _AnaSayfaUrunWidgetState();
+  final String baslik;
+  final double fiyat;
+  final bool favoriMi;
+  final String resimYolu;
+  final double indirimOrani;
+}
+
+// var baslik = "Adidas Ayakkabı";
+// double fiyat = 200;
+// bool favoriMi = false;
+// String resimYolu = "assets/Adidas1.png";
+// double indirimOrani = 0.2;
+
+class _AnaSayfaUrunWidgetState extends State<AnaSayfaUrunWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Image.asset(
+                  widget.resimYolu,
+                  fit: BoxFit.cover,
+                  width: 150,
+                ),
+                Positioned(
+                  top: 1,
+                  right: 0,
+                  child: Icon(Icons.favorite_border, color: Colors.white),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text(
+              widget.baslik,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Text(
+                  '\$${widget.fiyat}',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 8),
+                Text(
+                  '${widget.indirimOrani.toInt()}% OFF',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
+  }
+}
